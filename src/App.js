5310f29d5c5b156d6b2e6f9f2ce8c460c5d68d11
Kévin Marais <86494393+KvnMrs@users.noreply.github.com>
2021-12-04 18:11:51 +0100

    04 - installation react-router-dom / ajout des routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Contact from "./pages/Contact";
+import About from "./pages/About";
+import Knowleges from "./pages/Knowleges";
+import NotFound from "./pages/NotFound";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/contact" element={<Contact />} />
+        <Route exact path="/a-propos" element={<About />} />
+        <Route exact path="/competences" element={<Knowleges />} />
+        <Route element={<NotFound />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
