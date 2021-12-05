55bcaf22f57dbe0fd45b1555b60018de9038bc87
Kévin Marais <86494393+KvnMrs@users.noreply.github.com>
2021-12-05 17:13:04 +0100

    06 - création squelette page Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Home = () => {
-  return <>Page Accueil</>;
+  return (
+    <>
+      <div>
+        <h1>Bienvenue !</h1>
+      </div>
+
+      <div>
+        <h1>Kévin Marais</h1>
+        <h2>En formation Developpeur Web full stack</h2>
+      </div>
+      <div>
+        <Link to="/a-propos">
+          <button type="button">Continuer</button>
+        </Link>
+      </div>
+    </>
+  );
 };
 
 export default Home;
