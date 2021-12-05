f77bef7be0b9171c3193f3a6dda85a0a9d9801c4
Kévin Marais <86494393+KvnMrs@users.noreply.github.com>
2021-12-05 18:40:35 +0100

    08 - création squelette page CV

--- src/pages/Cv.jsx
+++ src/pages/Cv.jsx
@@ -1,7 +1,29 @@
 import React from "react";
 
 const Cv = () => {
-  return <>Page CV</>;
+  return (
+    <>
+      <div>
+        <h2>Expériences</h2>
+        <ul>
+          <li>exp</li>
+          <li>exp</li>
+          <li>exp</li>
+          <li>exp</li>
+          <li>exp</li>
+          <li>exp</li>
+        </ul>
+      </div>
+      <div>
+        <h2>Formations et diplômes</h2>
+        <ul>
+          <li>Formation</li>
+          <li>Formation</li>
+          <li>Formation</li>
+        </ul>
+      </div>
+    </>
+  );
 };
 
 export default Cv;
