cd83ebbeac913cee375934f3bf6525c72e355882
Kévin Marais <86494393+KvnMrs@users.noreply.github.com>
2021-12-05 17:54:01 +0100

    07 - création squelette page à propos

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,33 @@
 import React from "react";
 
 const About = () => {
-  return <>Page A propos</>;
+  return (
+    <>
+      <div>
+        <h2> En quelques mots..</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus quis
+          ex aliquam, volutpat ante sit amet, egestas erat. Mauris mollis urna
+          sit amet tincidunt aliquet. Mauris porta a turpis eleifend
+          ullamcorper. Curabitur et odio mattis, accumsan neque eget, pretium
+          enim. Phasellus mauris risus, pharetra nec urna sit amet, mattis
+          varius leo. Aliquam erat volutpat. Proin viverra vitae ex sit amet
+          malesuada. Pellentesque habitant morbi tristique senectus et netus et
+          malesuada fames ac turpis egestas. Vivamus fringilla quis neque ut
+          laoreet. Quisque elementum gravida turpis at eleifend.
+        </p>
+      </div>
+
+      <div>
+        <div>
+          <h2>Softs skills</h2>
+        </div>
+        <div>
+          <h2>Hards skills</h2>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default About;
