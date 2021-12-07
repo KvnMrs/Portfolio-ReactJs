fd77b9152aa9c449e44625d1367929ec41a1566a
Kévin Marais <86494393+KvnMrs@users.noreply.github.com>
2021-12-07 14:30:59 +0100

    10.1 - création datas / expériences / autre & coiffure

--- src/data/DataExperiences.js
+++ src/data/DataExperiences.js
@@ -1,42 +1,113 @@
-import logo_ScaOuest from "../assets/imgExperiences/logo_ScaOuest";
-import logo_FrenchPub from "../assets/imgExperiences/logo_FrenchPub";
-import logo_Opium from "../assets/imgExperiences/logo_Opium";
+// import logo_ScaOuest from "../imgExperiences/logo_ScaOuest.png";
+import logo_FrenchPub from "../assets/imgExperiences/logo_FrenchPub.png";
+import logo_Opium from "../assets/imgExperiences/logo_Opium.png";
 
 const dataCv = [
+  /********** RESTAURATION ***************/
   {
-    logo: { logo_ScaOuest },
-    date: " Mai 2021",
+    // img: logo_ScaOuest,
+    date: "Mai 2021",
     entreprise: "Sca-Ouest ",
     lieu: "Saint-Etienne de Montluc",
     statut: "Préparateur de commande",
     mission: "Préparations des commandes en entrepôt.",
+    categorie: "autre",
   },
   {
-    logo: {},
-    date: " Avril 2019 à Février 2020",
-    entreprise: " L'Octopus ",
+    // img: {},
+    date: "Avril 2019 à Février 2020",
+    entreprise: "L'Octopus ",
     lieu: "Nantes",
     statut: "Chef de rang ",
     mission:
       "Préparation du service, accueil, prise de commande, encaissement, gestion d'un rang.",
+    categorie: "restauration",
   },
   {
-    logo: { logo_FrenchPub },
+    img: logo_FrenchPub,
     date: " Septembre 2018 à Mars 2019",
     entreprise: "Le French Pub ",
     lieu: "Saint-Sébastien",
     statut: "Serveur",
     mission:
       "Préparation de la salle et du service, envoi des commandes, néttoyage de la salle",
+    categorie: "restauration",
   },
   {
-    logo: { logo_Opium },
+    img: logo_Opium,
     date: "Juin 2018 à Septembe 2018",
     entreprise: "L'Opium ",
     lieu: "Nantes",
     statut: "Serveur / Barman",
     mission:
       "Service et conception des boissons au bar, services des plats en salle",
+    categorie: "restauration",
+  },
+
+  {
+    // img: logo_Opium,
+    date: "Septembre 2017 à Juin 2018",
+    entreprise: "La Coquille ",
+    lieu: "Nantes",
+    statut: "Buraliste",
+    mission: "Vente des produits présents, gestion des conflits",
+    categorie: "autre",
+  },
+  {
+    // img: logo_Opium,
+    date: "Juin 2017 à Septembe 2017",
+    entreprise: "Tipiak ",
+    lieu: "Saint-Herblain",
+    statut: "Agent de production",
+    mission:
+      "Réalisation des commandes quotidienne, respect des protocole d'hygienne et de sécurité",
+    categorie: "autre",
+  },
+  {
+    // img: logo_Opium,
+    date: "Février 2017 à Juin 2018",
+    entreprise: "Sam's Barber ",
+    lieu: "Dublin",
+    statut: "Barbier",
+    mission: "Stage en immersion dans le but d'apprendre l'Anglais",
+    categorie: "coiffure",
+  },
+  {
+    // img: logo_Opium,
+    date: "Novembre 2016 à Février 2017",
+    entreprise: "Shampoo",
+    lieu: "Saint-Sebastien",
+    statut: "Barbier",
+    mission:
+      "Recherche des besoins client, conseil, realisation des coupes et diférrentes techniques de coloration   ",
+    categorie: "coiffure",
+  },
+  {
+    // img: logo_Opium,
+    date: "Septembre 2014 à Juin 2016",
+    entreprise: "Saint-Algue",
+    lieu: "Nantes",
+    statut: "Apprenti - BP",
+    mission: "Apprentissage des différentes techniques de coupe et coloration",
+    categorie: "coiffure",
+  },
+  {
+    // img: logo_Opium,
+    date: "Septembre 2012 à Juin 2014",
+    entreprise: "Saint-Algue",
+    lieu: "Saint-Sebastien",
+    statut: "Apprenti - CAP",
+    mission: "Apprentissage des différentes technique de coupe et coloration",
+    categorie: "coiffure",
+  },
+  {
+    // img: logo_Opium,
+    date: "Septembre 2010 à Juin 2012",
+    entreprise: "Salon Saint-Joseph",
+    lieu: "Saint Joseph de Porterie",
+    statut: "Apprenti - CAP",
+    mission: "Apprentissage des différentes technique de coupe et coloration",
+    categorie: "coiffure",
   },
 ];
 
