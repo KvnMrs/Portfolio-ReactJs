cb0271365d5dc9dc8e5f94213cedb7b338fd97c0
Kévin Marais <86494393+KvnMrs@users.noreply.github.com>
2021-12-27 23:05:41 +0100

    feat : ajout data + composant Skills

---
+++ src/components/uniques/Skills.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+import dataSkills from "../../datas/DatasSkills";
+
+const Skills = () => {
+  return (
+    <>
+      <div className="w-11/12 bg-dark_blue scrollbar-hide gradiant-dark rounded-2xl overflow-scroll h-5/6 mx-auto my-6 px-5 py-10  shadow-2xl ">
+        <h1 className="text-3xl text-white mt-8">Atouts</h1>
+        <div className="flex place-content-center my-5">
+          {dataSkills
+            .filter((atout) => atout.catégorie === "Atout")
+            .map((el) => (
+              <div className="grid-cols-1 mx-auto my-5">
+                <div className="mx-5  mb-3 text-xl text-white object-center">
+                  {el.element}
+                </div>
+                <img src={el.image} className="w-20 h-16 mx-auto" />
+              </div>
+            ))}
+        </div>
+        <h1 className="text-3xl text-white mt-8">Compétences</h1>
+        <div className="grid grid-cols-3 gap-4 place-content-center my-5 flew-wrap">
+          {dataSkills
+            .filter((compétence) => compétence.catégorie === "Compétence")
+            .map((el) => (
+              <div className="grid-cols-1 mx-auto my-5">
+                <div className="mx-5  mb-3 text-xl text-white object-center">
+                  {el.element}
+                </div>
+                <img src={el.image} className="w-20 h-16 mx-auto" />
+              </div>
+            ))}
+        </div>
+
+        <h1 className="text-3xl text-white mt-8">Outils</h1>
+        <div className="grid grid-cols-3 gap-4 place-content-center my-5 flew-wrap">
+          {dataSkills
+            .filter((outil) => outil.catégorie === "Outil")
+            .map((el) => (
+              <div className="grid-cols-1 mx-auto my-5">
+                <div className="mx-5  mb-3 text-xl text-white object-center">
+                  {el.element}
+                </div>
+                <img src={el.image} className="w-20 h-16 mx-auto" />
+              </div>
+            ))}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Skills;
---
+++ src/datas/DatasSkills.jsx
@@ -0,0 +1,121 @@
+import img_tailwindcss from "../assets/imgHardSkills/Tailwindcss.png";
+import img_CSS from "../assets/imgHardSkills/css-3.png";
+import img_HTML from "../assets/imgHardSkills/html.png";
+import img_JS from "../assets/imgHardSkills/JavaScript.png";
+import img_MySQL from "../assets/imgHardSkills/MySQL.png";
+import img_NodeJs from "../assets/imgHardSkills/NodeJs.png";
+import img_ReactJs from "../assets/imgHardSkills/ReactJs.png";
+import img_ESlint from "../assets/imgTool/ESlint.png";
+import img_Git from "../assets/imgTool/Git.png";
+import img_GitHub from "../assets/imgTool/GitHub.png";
+import img_VScode from "../assets/imgTool/VScode.png";
+import img_Figma from "../assets/imgTool/Figma.png";
+import img_Airtable from "../assets/imgTool/Airtable.png";
+import img_autonome from "../assets/imgSoftSkills/autonome.png";
+import img_curieux from "../assets/imgSoftSkills/curieux.png";
+import img_esprit_equipe from "../assets/imgSoftSkills/espritEquipe.png";
+import img_logique from "../assets/imgSoftSkills/logique.png";
+import img_perseverant from "../assets/imgSoftSkills/perseverant.png";
+
+const dataSkills = [
+  /********** SOFTS SKILLS ***************/
+
+  {
+    image: img_esprit_equipe,
+    catégorie: "Atout",
+    element: "Esprit d'équipe",
+  },
+  {
+    image: img_autonome,
+    catégorie: "Atout",
+    element: "Autonome",
+  },
+  {
+    image: img_perseverant,
+    catégorie: "Atout",
+    element: "Perséverant",
+  },
+  {
+    image: img_logique,
+    catégorie: "Atout",
+    element: "Logique",
+  },
+  {
+    image: img_curieux,
+    catégorie: "Atout",
+    element: "Curieux",
+  },
+
+  /********** HARDS SKILLS ***************/
+
+  {
+    catégorie: "Compétence",
+    element: "HTML5",
+    image: img_HTML,
+  },
+  {
+    catégorie: "Compétence",
+    element: "CSS3",
+    image: img_CSS,
+  },
+  {
+    catégorie: "Compétence",
+    element: "JavaScript",
+    image: img_JS,
+  },
+  {
+    catégorie: "Compétence",
+    element: "ReactJs",
+    image: img_ReactJs,
+  },
+  {
+    catégorie: "Compétence",
+    element: "NodeJs",
+    image: img_NodeJs,
+  },
+  {
+    catégorie: "Compétence",
+    element: "TailwindCSS",
+    image: img_tailwindcss,
+  },
+  {
+    catégorie: "Compétence",
+    element: "Mysql",
+    image: img_MySQL,
+  },
+
+  /********** OUTILS ***************/
+
+  {
+    catégorie: "Outil",
+    element: "GitHub",
+    image: img_GitHub,
+  },
+  {
+    catégorie: "Outil",
+    element: "Git",
+    image: img_Git,
+  },
+  {
+    catégorie: "Outil",
+    element: "ESlint",
+    image: img_ESlint,
+  },
+  {
+    catégorie: "Outil",
+    element: "Visual studio code",
+    image: img_VScode,
+  },
+  {
+    catégorie: "Outil",
+    element: "Figma",
+    image: img_Figma,
+  },
+  {
+    catégorie: "Outil",
+    element: "Airtable",
+    image: img_Airtable,
+  },
+];
+
+export default dataSkills;
