66b6c0a502cc3ba0bcdd192b6d9e7c67414669fc
Kévin Marais <86494393+KvnMrs@users.noreply.github.com>
2021-12-28 16:51:43 +0100

    style : correction orthographe style

--- src/components/uniques/HardSkills.jsx
+++ src/components/uniques/HardSkills.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import dataSkills from "../../datas/DatasSkills";
 
-const Skills = () => {
+const HardSkills = () => {
   return (
     <>
       <div className="w-11/12 bg-dark_blue scrollbar-hide gradiant-dark rounded-2xl overflow-scroll h-5/6 mx-auto my-6 px-5 py-10  shadow-2xl ">
         <h1 className="text-3xl text-white mt-8">Compétences</h1>
-        <div className="grid grid-cols-3 gap-4 place-content-center my-5 flew-wrap">
+        <div className="grid grid-cols-3 gap-4 place-content-center my-5 flex-wrap">
           {dataSkills
             .filter((compétence) => compétence.catégorie === "Compétence")
             .map((el) => (
@@ -20,12 +20,12 @@ const Skills = () => {
         </div>
 
         <h1 className="text-3xl text-white mt-8">Outils</h1>
-        <div className="grid grid-cols-3 gap-4 place-content-center my-5 flew-wrap">
+        <div className="grid grid-cols-3 gap-4 place-content-center my-5 flex-wrap">
           {dataSkills
             .filter((outil) => outil.catégorie === "Outil")
             .map((el) => (
               <div className="grid-cols-1 mx-auto my-5">
-                <div className="mx-5  mb-3 text-xl text-white object-center">
+                <div className="mx-5 mb-3 text-xl text-white object-center">
                   {el.element}
                 </div>
                 <img src={el.image} className="w-20 h-16 mx-auto" />
@@ -37,4 +37,4 @@ const Skills = () => {
   );
 };
 
-export default Skills;
+export default HardSkills;
