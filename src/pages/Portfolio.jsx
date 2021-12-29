9acafde49870652d6f3ce684768b4dcf3c87b6e6
Kévin Marais <86494393+KvnMrs@users.noreply.github.com>
2021-12-30 00:41:37 +0100

    style : responsive page "Portfolio"

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,16 +1,12 @@
 import React from "react";
 import Portfolio from "../components/uniques/Portfolio";
+import Button from "../components/communs/Button";
 
 const Portefolio = () => {
   return (
     <>
       <div className="w-full text-center mx-auto py-5 list-none h-screen place-content-center  ">
-        <button
-          type="button"
-          className="text-2xl m-2 text-white border-2 border-gray-200 bg-dark_blue hover:bg-white hover:text-dark_blue py-1 px-7 rounded-2xl  shadow-2xl"
-        >
-          Projets Wild Code School
-        </button>
+        <Button>Projets Wild Code School</Button>
         <Portfolio />
       </div>
     </>
